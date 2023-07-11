22e6eb72ab969260697b5ef31c593cfe7bfa4795
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-07-11 11:24:40 -0600

    Declare variables

---
+++ arrays.rb
@@ -0,0 +1,13 @@
+puts "Declare a variable that stores an Array of at least 4 Strings"
+names = ["Shawn", "Carol", "Cynthia", "Charlie"]
+puts names
+
+puts "Declare a variable that stores an Array of at least 4 Integers"
+ages = [23, 24, 27, 26]
+puts ages
+puts "Declare a variable that stores an Array of at least 4 Floats"
+account_balances = [23.06, 27.68, 37.49, 32.64]
+puts account_balances
+puts "Declare a variable that stores an Array of at least 4 Booleans"
+has_pet = [true, true, false, true]
+puts has_pet
